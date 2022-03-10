85c953a15e84b7a9eca89b0097c11284d06510e1
Yulia Dobrova <dobrova2009@gmail.com>
2022-03-10 09:21:33 +0200

    dnd added

---
+++ src/components/dragAndDrop/DragAndDrop.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import './DragAndDrop.css';
+
+const DragAndDrop = () => {
+  const initialState = [
+    { id: 1, order: 3, text: 'Card-3' },
+    { id: 2, order: 1, text: 'Card-1' },
+    { id: 3, order: 2, text: 'Card-2' },
+    { id: 4, order: 4, text: 'Card-4' },
+  ];
+  const [cardList, setCardList] = useState(initialState);
+  const [currentCard, setCurrentCard] = useState(null);
+
+  function dragStartHandler(e, card) {
+    console.log('drag', card);
+    setCurrentCard(card);
+    e.target.style.borderColor = '#61dafb';
+  }
+
+  function dragEndHandler(e) {
+    e.target.style.background = 'transparent';
+    e.target.style.borderColor = 'rgba(139, 152, 178, 0.95)';
+  }
+  function dragOverHandler(e) {
+    e.preventDefault();
+    e.target.style.background = 'rgba(139, 152, 178, 0.25)';
+  }
+
+  function dropHandler(e, card) {
+    e.preventDefault();
+    setCardList(
+      cardList.map(c => {
+        if (c.id === card.id) {
+          return { ...c, order: currentCard.order };
+        }
+        if (c.id === currentCard.id) {
+          return { ...c, order: card.order };
+        }
+        return c;
+      }),
+    );
+    e.target.style.background = 'transparent';
+  }
+
+  const sortCards = (a, b) => {
+    if (a.order > b.order) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
+
+  return (
+    <div className="DragAndDropWrapper">
+      {cardList.sort(sortCards).map(card => (
+        <div
+          className="DragAndDropCard"
+          draggable={true}
+          onDragStart={e => dragStartHandler(e, card)}
+          onDragLeave={e => dragEndHandler(e)}
+          onDragEnd={e => dragEndHandler(e)}
+          onDragOver={e => dragOverHandler(e)}
+          onDrop={e => dropHandler(e, card)}
+        >
+          {card.text}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DragAndDrop;
--- src/pages/FunctionalPage.jsx
+++ src/pages/FunctionalPage.jsx
@@ -7,6 +7,7 @@ import Timer from '../components/timer/Timer';
 import TimerBitrhday from '../components/timerBirthday/TimerBitrhday';
 import ColorSwitcher from '../components/colorSwitcher/ColorSwitcher';
 import ColorPicker from '../components/colorPicker/ColorPicker';
+import DragAndDrop from '../components/dragAndDrop';
 
 const FunctionalPage = () => {
   return (
@@ -17,6 +18,8 @@ const FunctionalPage = () => {
         title="Click the accordion below to expand/collapse the accordion content. "
         description="This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow."
       />
+      <hr style={{ margin: 15 }} />
+      <DragAndDrop />
       <hr style={{ margin: 15 }} />
       <CommentSlider />
       <hr style={{ marginLeft: 15, marginRight: 15, marginTop: 50 }} />
