b0b47434e3b92f6f3e9e99ce7caf180b15864145
yuliiadobrova <dobrova2009@gmail.com>
2024-03-03 15:39:23 -0800

    modal esc close added

--- src/components/shared/modal/Modal.jsx
+++ src/components/shared/modal/Modal.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import './Modal.css';
 
 const Modal = ({ active, setActive, children }) => {
+  const onHandleClick = () => setActive(false);
+
+  const handleEscKey = event => {
+    if (event.key === 'Escape') {
+      setActive(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keyup', handleEscKey, false);
+
+    return () => {
+      document.removeEventListener('keyup', handleEscKey, false);
+    };
+  }, [handleEscKey]);
+
   return (
     <div
       className={active ? 'modal active' : 'modal'}
-      onClick={() => {
-        setActive(false);
-      }}
+      onClick={onHandleClick}
+      onKeyUp={handleEscKey}
     >
       <div
         className={active ? 'modal-content active' : 'modal-content'}
